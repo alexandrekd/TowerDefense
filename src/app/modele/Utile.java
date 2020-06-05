2edc466573a534a830da04e6fd30687b3f7e10d4
mateo <mateolaville@hotmail.fr>
2020-06-05 10:30:59 +0200

    Merge branch 'master' of https://github.com/alexandrekd/TowerDefense
     Conflicts:
    	src/app/modele/Utile.java

--- src/app/modele/Utile.java
+++ src/app/modele/Utile.java
@@ -42,14 +42,17 @@ public class Utile {
     }
 
     // Cree et retourne des ennemis positionnes sur la case depart de maniere aleatoire
-    public static Acteur creerEnnemi (Environnement env){
+    public static Attaquant creerEnnemi (Environnement env, int type){
         int random = (int) (Math.random() * env.getMap().parallelStream().filter(n-> n/900 == 1).collect(Collectors.toList()).size());
         int count = 0;
-        Acteur attaquant = null;
+        Attaquant attaquant = null;
         for (int i = 0 ; i < env.getMap().size(); i++){
             if(env.getMap().get(i)/900 ==1){
                 if (count == random){
-                    attaquant = new Theo(env,Utile.toX(i),Utile.toPixel(Utile.toY(i)) + 10 +(int) (Math.random()*30));
+                    if(type == 1)
+                        attaquant = new Normal(env,Utile.toX(i),Utile.toPixel(Utile.toY(i)) + 10 +(int) (Math.random()*30));
+                    if(type == 2)
+                        attaquant = new Theo(env,Utile.toX(i),Utile.toPixel(Utile.toY(i)) + 10 +(int) (Math.random()*30));
                     count = 0;
                 }
                 else
